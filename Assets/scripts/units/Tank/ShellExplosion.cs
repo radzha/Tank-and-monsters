92dee73075b659db80fd002c4807821db78c27f0
Dzhangir Rakhmatullin <developer4.lingplay@gmail.com>
2016-11-17 15:31:28 +0500

    Косметический рефакторинг

--- Assets/scripts/units/Tank/ShellExplosion.cs
+++ Assets/scripts/units/Tank/ShellExplosion.cs
@@ -1,21 +1,17 @@
 using UnityEngine;
 using Progress;
 
+/// <summary>
+/// Класс, отвечающий за взрыв и урон от снаряда.
+/// </summary>
 public class ShellExplosion : MonoBehaviour {
 	public LayerMask m_TankMask;
-	// Used to filter what the explosion affects, this should be set to "Players".
 	public ParticleSystem m_ExplosionParticles;
-	// Reference to the particles that will play on explosion.
 	public AudioSource m_ExplosionAudio;
-	// Reference to the audio that will play on explosion.
 	public float m_MaxDamage = 100f;
-	// The amount of damage done if the explosion is centred on a tank.
 	public float m_ExplosionForce = 1000f;
-	// The amount of force added to a tank at the centre of the explosion.
 	public float m_MaxLifeTime = 2f;
-	// The time in seconds before the shell is removed.
 	public float m_ExplosionRadius = 5f;
-	// The maximum distance away from the explosion tanks can be and are still affected.
 
 	public Unit OwnerUnit{ get; set; }
 
@@ -33,7 +29,6 @@ public class ShellExplosion : MonoBehaviour {
 			return;
 		}
 
-		// Add an explosion force.
 		targetRigidbody.AddExplosionForce(m_ExplosionForce, transform.position, m_ExplosionRadius);
 
 		var unit = targetRigidbody.GetComponent<Progress.Unit>();
@@ -41,19 +36,11 @@ public class ShellExplosion : MonoBehaviour {
 		float damage = OwnerUnit.Settings.Attack;
 		unit.TakeDamage(OwnerUnit, damage);
 
-		// Unparent the particles from the shell.
 		m_ExplosionParticles.transform.parent = null;
-
-		// Play the particle system.
 		m_ExplosionParticles.Play();
-
-		// Play the explosion sound effect.
 		m_ExplosionAudio.Play();
 
-		// Once the particles have finished, destroy the gameobject they are on.
 		Destroy(m_ExplosionParticles.gameObject, m_ExplosionParticles.duration);
-
-		// Destroy the shell.
 		Destroy(gameObject);
 	}
 }
